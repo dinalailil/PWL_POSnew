7b27220fa270c82ea49b06a9430998051c1d202c
dinalailil <dinarullailil26@gmail.com>
2025-03-14 21:27:17 +0700

    Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,9 +7,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller{
-    public function index(){
-
-        $user = UserModel::where('username','manager_3')->firstOrFail();
-        return view('user',['data' => $user]);
+    public function index() {
+        $jumlahPengguna = UserModel::where('level_id', 2)->count();
+        return view('user', ['jumlahPengguna' => $jumlahPengguna]);
     }
+    
+    // public function index(){
+
+    //     $user = UserModel::where('level_id',2)->get();
+
+    //     return view('user',['data' => $user]);
+    // }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,18 +4,59 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $jumlahPengguna }}</td>
+        </tr>
+    </table>
+</body>
+</html>
+
+{{-- <!DOCTYPE html>
+<html>
+<body>
+    <h1>Data ID Level Pengguna</h1>
+    @if($data->count() > 0)
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID Level Pengguna</th>
+            </tr>
+            @foreach($data as $user)
+            <tr>
+                <td>{{ $user->level_id }}</td>  
+            </tr>
+            @endforeach
+        </table>
+    @else
+        <p>Tidak ada data user dengan level ID 2.</p>
+    @endif
+</body>
+</html> --}}
+
+{{-- <!DOCTYPE html>
+<html>
+<body>
+    <h1>Data User</h1>
+    @if($data->count() > 0)
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
         </tr>
+        @foreach($data as $user)
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td>{{ $user->user_id }}</td>
+            <td>{{ $user->username }}</td>
+            <td>{{ $user->nama }}</td>
+            <td>{{ $user->level_id }}</td>
         </tr>
-
+        @endforeach
     </table>
+    @else
+    <p>Tidak ada data user dengan level ID 2.</p>
+@endif
 </body>
-</html>
+</html> --}}
