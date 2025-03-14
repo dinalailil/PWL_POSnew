da6dde0e7fff05976f54264c44d4dbb7681faf5e
dinalailil <dinarullailil26@gmail.com>
2025-03-14 20:58:46 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,15 +8,10 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller{
     public function index(){
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_3',
-            'nama' => 'Manager 3',
-            'password' => hash::make('12345')
-        ];
-        UserModel::create($data);
 
-        $user = UserModel::all();
+        $user = UserModel::findOr(20,['username','nama'],function() {
+            abort(404);
+        });
         return view('user',['data' => $user]);
     }
 }
