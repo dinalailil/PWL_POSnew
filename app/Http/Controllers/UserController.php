306966f7ce2c6e8fe4f265734d9c4aabe1dfa1b8
dinalailil <dinarullailil26@gmail.com>
2025-04-08 15:35:28 +0700

    filtering

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,9 +23,12 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
+        $level = LevelModel::all(); // ambil data level untuk filter level
+
         return view('user.index', [
             'breadcrumb' => $breadcrumb,
             'page'       => $page,
+            'level'      => $level,
             'activeMenu' => $activeMenu
         ]);
     }
@@ -34,7 +37,12 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
-    
+        
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
     return DataTables::of($users)
         // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
         ->addIndexColumn()
