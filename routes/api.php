22bcea3279c66c59677add75f67644f292cd9954
dinalailil <dinarullailil26@gmail.com>
2025-05-23 20:53:16 +0700

    Jobsheet10prak3

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\RegisterController;
-use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,12 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 });
+
